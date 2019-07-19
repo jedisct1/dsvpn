5709e21aa7d21255a8cf0a370260ee0577858c32
Frank Denis <github@pureftpd.org>
2019-07-19 02:39:25 +0200

    safe_{read,write}: return -1 on timeout/errors

--- src/dsvpn.c
+++ src/dsvpn.c
@@ -104,16 +104,15 @@ safe_write(const int fd, const void* const buf_, size_t count,
                 pfd.fd     = fd;
                 pfd.events = POLLOUT;
                 if (poll(&pfd, (nfds_t) 1, timeout) <= 0) {
-                    goto ret;
+                    return (ssize_t) -1;
                 }
             } else if (errno != EINTR) {
-                goto ret;
+                return (ssize_t) -1;
             }
         }
         buf += written;
         count -= written;
     }
-ret:
     return (ssize_t)(buf - (const char*) buf_);
 }
 
@@ -122,7 +121,7 @@ safe_read(const int fd, void* const buf_, size_t count, const int timeout)
 {
     struct pollfd  pfd;
     unsigned char* buf    = (unsigned char*) buf_;
-    ssize_t        readnb = -1;
+    ssize_t        readnb = (ssize_t) -1;
 
     while (readnb != 0 && count > (ssize_t) 0) {
         while ((readnb = read(fd, buf, count)) < (ssize_t) 0) {
@@ -130,16 +129,15 @@ safe_read(const int fd, void* const buf_, size_t count, const int timeout)
                 pfd.fd     = fd;
                 pfd.events = POLLIN;
                 if (poll(&pfd, (nfds_t) 1, timeout) <= 0) {
-                    goto ret;
+                    return (ssize_t) -1;
                 }
             } else if (errno != EINTR) {
-                goto ret;
+                return (ssize_t) -1;
             }
         }
         count -= readnb;
         buf += readnb;
     }
-ret:
     return (ssize_t)(buf - (unsigned char*) buf_);
 }
 
