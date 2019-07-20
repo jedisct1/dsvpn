7f4c1d2ab91c69f2080b9f0a9e5e7969ba6b6f9f
Frank Denis <github@pureftpd.org>
2019-07-20 08:58:14 +0200

    unbreak

--- include/os.h
+++ include/os.h
@@ -3,6 +3,14 @@
 
 #include "dsvpn.h"
 
+ssize_t safe_read(const int fd, void *const buf_, size_t count, const int timeout);
+
+ssize_t safe_write(const int fd, const void *const buf_, size_t count, const int timeout);
+
+ssize_t safe_read_partial(const int fd, void *const buf_, const size_t max_count);
+
+ssize_t safe_write_partial(const int fd, void *const buf_, const size_t max_count);
+
 typedef struct Cmds {
     const char *const *set;
     const char *const *unset;
--- src/os.c
+++ src/os.c
@@ -1,6 +1,78 @@
 #include "os.h"
 #include "dsvpn.h"
 
+extern volatile sig_atomic_t exit_signal_received;
+
+ssize_t safe_read(const int fd, void *const buf_, size_t count, const int timeout)
+{
+    struct pollfd  pfd;
+    unsigned char *buf    = (unsigned char *) buf_;
+    ssize_t        readnb = (ssize_t) -1;
+
+    while (readnb != 0 && count > (ssize_t) 0) {
+        while ((readnb = read(fd, buf, count)) < (ssize_t) 0) {
+            if (errno == EAGAIN) {
+                pfd.fd     = fd;
+                pfd.events = POLLIN;
+                if (poll(&pfd, (nfds_t) 1, timeout) <= 0) {
+                    return (ssize_t) -1;
+                }
+            } else if (errno != EINTR || exit_signal_received) {
+                return (ssize_t) -1;
+            }
+        }
+        count -= readnb;
+        buf += readnb;
+    }
+    return (ssize_t)(buf - (unsigned char *) buf_);
+}
+
+ssize_t safe_write(const int fd, const void *const buf_, size_t count, const int timeout)
+{
+    struct pollfd pfd;
+    const char *  buf = (const char *) buf_;
+    ssize_t       written;
+
+    while (count > (size_t) 0) {
+        while ((written = write(fd, buf, count)) < (ssize_t) 0) {
+            if (errno == EAGAIN) {
+                pfd.fd     = fd;
+                pfd.events = POLLOUT;
+                if (poll(&pfd, (nfds_t) 1, timeout) <= 0) {
+                    return (ssize_t) -1;
+                }
+            } else if (errno != EINTR || exit_signal_received) {
+                return (ssize_t) -1;
+            }
+        }
+        buf += written;
+        count -= written;
+    }
+    return (ssize_t)(buf - (const char *) buf_);
+}
+
+ssize_t safe_read_partial(const int fd, void *const buf_, const size_t max_count)
+{
+    unsigned char *const buf = (unsigned char *) buf_;
+    ssize_t              readnb;
+
+    while ((readnb = read(fd, buf, max_count)) < (ssize_t) 0 && errno == EINTR &&
+           !exit_signal_received)
+        ;
+    return readnb;
+}
+
+ssize_t safe_write_partial(const int fd, void *const buf_, const size_t max_count)
+{
+    unsigned char *const buf = (unsigned char *) buf_;
+    ssize_t              writenb;
+
+    while ((writenb = write(fd, buf, max_count)) < (ssize_t) 0 && errno == EINTR &&
+           !exit_signal_received)
+        ;
+    return writenb;
+}
+
 #ifdef __linux__
 int tun_create(char if_name[IFNAMSIZ], const char *wanted_name)
 {
